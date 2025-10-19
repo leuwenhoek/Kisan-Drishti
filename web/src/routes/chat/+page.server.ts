fbd5606405d3259038be1bb0a85ed0d7f5cb6039
Priyanshu mahala <mahalapriyanshu35@gmail.com>
2025-10-19 07:33:38 +0000

    ADD gemini API

--- web/src/routes/chat/+page.server.ts
+++ web/src/routes/chat/+page.server.ts
@@ -2,7 +2,6 @@ import { GoogleGenAI } from "@google/genai";
 import { GEMINI_API_KEY } from "$env/static/private";
 import type { Actions, RequestEvent } from "./$types";
 
-// The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
 
 export const actions: Actions = {
